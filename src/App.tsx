acbcb79ae226d2cb3d6a4ac8f2266f1b705e9309
Guilherme Bacca <guilherme.drbacca@gmail.com>
2023-10-10 01:32:59 -0300

    commit final funcionando pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { CartContextProvider } from './Context/CartContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
+      <HashRouter>
         <CartContextProvider>
           <Router />
         </CartContextProvider>
-      </BrowserRouter>
+      </HashRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
